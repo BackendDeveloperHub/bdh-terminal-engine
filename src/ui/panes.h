d097f94211b6a543938e3ef6e1319fe146722905
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-02 22:50:30 +0530

    Update window size limits to 150 rows and 300 columns
    Increased maximum window dimensions for better layout support.

--- src/ui/panes.h
+++ src/ui/panes.h
@@ -4,23 +4,25 @@
 
 #include "engine/screen.h"
 
-#define WIN_MAX_ROWS 25
-#define WIN_MAX_COLS 80
+// இந்த இரண்டு எண்களையும் பெரிய அளவாக மாற்றுங்கள்:
+#define WIN_MAX_ROWS 150  // <-- 150
+#define WIN_MAX_COLS 300  // <-- 300 (இதுதான் முக்கியம்!)
 
 typedef struct {
     int id;
-    int x, y;          
-    int width, height; 
-    int z_index;       
-    char title[64];    
-    int is_active;     
-    int cur_r, cur_c;  
-    char text[WIN_MAX_ROWS][WIN_MAX_COLS]; // <-- விண்டோவின் சொந்த மெமரி (Backing Store)
+    int x, y;
+    int width, height;
+    int z_index;
+    int is_active;
+    char title[64];
+    char text[WIN_MAX_ROWS][WIN_MAX_COLS];
+    int cur_r, cur_c;
 } FloatingWindow;
 
 FloatingWindow* window_create(int id, int x, int y, int width, int height, const char* title, int z_index);
 void window_draw(VirtualScreen *scr, FloatingWindow *win);
 void window_put_char(VirtualScreen *scr, FloatingWindow *win, char ch);
+void window_scroll_up(FloatingWindow *win);
 void window_destroy(FloatingWindow *win);
 
 #endif // BDH_PANES_H
