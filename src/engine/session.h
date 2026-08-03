63364eec8eaa6b0a1b81ba87681d4a6e1317fdac
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-03 22:35:39 +0530

    Add session management header for terminal sessions

---
+++ src/engine/session.h
@@ -0,0 +1,24 @@
+// include/engine/session.h - BDH Terminal Session Management Header
+#ifndef SESSION_H
+#define SESSION_H
+
+#include <sys/types.h>
+#include "ui/panes.h"
+#include "engine/parser.h"
+
+#define MAX_SESSIONS 6
+
+typedef struct {
+    int id;
+    int master_fd;
+    pid_t pid;
+    FloatingWindow *win;
+    AnsiParser *parser;
+    int is_alive;
+} TerminalSession;
+
+// Session Management Functions:
+void sessions_init_all(TerminalSession sessions[MAX_SESSIONS], char *shell_argv[], int pty_rows, int pty_cols, int scr_cols, int scr_rows, TabBar *tab_bar, char *tab_names[]);
+void sessions_cleanup_all(TerminalSession sessions[MAX_SESSIONS], TabBar *tab_bar, StatusBar *status_bar, TokenScanner *token_scanner, Clipboard *engine_cb, VirtualScreen *scr);
+
+#endif
