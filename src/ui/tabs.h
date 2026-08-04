a28410b32306425026cecfe7df1189ecfc26c3d7
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-05 00:16:42 +0530

    Increase max tabs and add tab overlay function
    Increased the maximum number of tabs from 6 to 20 and added a new function for rendering a tab overlay dashboard.

--- src/ui/tabs.h
+++ src/ui/tabs.h
@@ -1,10 +1,10 @@
-// src/ui/tabs.h - BDH Terminal Engine Tab Bar UI
+// src/ui/tabs.h - BDH Terminal Engine Tab Bar UI & Overlay Manager
 #ifndef BDH_TABS_H
 #define BDH_TABS_H
 
 #include "engine/screen.h"
 
-#define MAX_TABS 6
+#define MAX_TABS 20      // 6-லிருந்து 20 அல்லது 50 டேப்களுக்கு உயர்த்திக்கொள்ளலாம்
 #define TAB_TITLE_LEN 32
 
 typedef struct {
@@ -20,11 +20,14 @@ typedef struct {
     int active_idx;
 } TabBar;
 
-// Tab Bar பங்க்ஷன்கள்
+// 1. Existing Tab Bar Functions
 TabBar* tabs_create(void);
 int tabs_add(TabBar *bar, const char *title);
 void tabs_set_active(TabBar *bar, int index);
 void tabs_draw(VirtualScreen *scr, TabBar *bar, int row);
 void tabs_destroy(TabBar *bar);
+
+// 2. --- ADDED: Tab Overlay Dashboard Function (git status style overview) ---
+void render_tab_overlay(VirtualScreen *scr, TabBar *bar);
 
 #endif // BDH_TABS_H
