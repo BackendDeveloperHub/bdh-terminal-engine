591bab689b7521838aac5d4f249399bfe46c2b38
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-04 22:39:10 +0530

    Add screen_put_char_color function
    Added function to place a character with foreground color in the virtual screen.

--- src/engine/screen.h
+++ src/engine/screen.h
@@ -1,4 +1,4 @@
-// src/engine/screen.h
+// src/engine/screen.h - BDH Virtual Screen Buffer Header (Updated)
 #ifndef BDH_SCREEN_H
 #define BDH_SCREEN_H
 
@@ -24,5 +24,8 @@ VirtualScreen* screen_create(int rows, int cols);
 void screen_destroy(VirtualScreen *screen);
 void screen_clear(VirtualScreen *screen);
 void screen_put_char(VirtualScreen *screen, int row, int col, char ch);
+
+// --- ADDED: செல் (Cell) மெமரியில் எழுத்து மற்றும் நிறத்தை (Foreground Color) ஒன்றாகப் பதிவு செய்யும் பங்க்ஷன் ---
+void screen_put_char_color(VirtualScreen *screen, int row, int col, char ch, int fg_color);
 
 #endif // BDH_SCREEN_H
