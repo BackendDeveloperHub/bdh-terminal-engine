59ae26e89df2c9da97375bfdf3e86f3c98d6389a
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-10 05:58:40 +0530

    Bump version from 0.0.1 to 0.0.2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ class PostInstallCommand(install):
 
 setup(
     name="bdh-terminal-engine",
-    version="0.0.1",
+    version="0.0.2",
     description="A 100% Pure Linux CLI Terminal Multiplexer written in C.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
